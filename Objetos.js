1c363a0b0ea6f2109f55875f339f1a0e838b4147
garzo94 <alexgarzo25@gmail.com>
2022-06-04 11:53:00 -0600

    first commit

---
+++ Objetos.js
@@ -0,0 +1,6 @@
+const myList =  new Array('Alex',28, true, new Date("2/13/94"), {
+    titulo:"Dont Remember",
+    autor: "Jonh Seeck",
+    fecha: "12/10/98",
+    url: "www.mybooks/dontremember.com"
+})
